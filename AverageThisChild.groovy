280205be41bcf710037b9b9bc261c7854127c9e4
c steele <csteele@pandoradigital.com>
2019-07-09 12:04:02 -0700

    v1.1

--- AverageThisChild.groovy
+++ AverageThisChild.groovy
@@ -15,6 +15,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+ public static String version()      {  return "v1.1"  }
 
 definition (
 	name: "AverageThisChild",
@@ -36,12 +37,6 @@ preferences
 }
 
 
-// App Version   ***** with great thanks and acknowlegment to Cobra (CobraVmax) for his original version checking code ********
-def setVersion() {
-	state.version = "1.0"
-	state.InternalName = "AverageThisChild"
-	state.Type = "Application"
-}
 
 def subscribeSelected() {
 	if (debugOutput) log.debug "subscribeSelected: $illumSensors"
@@ -54,6 +49,7 @@ def subscribeSelected() {
 }
 
 def illumHandler(evt) {
+	def NSample = numberOption as Integer
 	if (debugOutput) log.debug "illumHandler: $evt.device, $evt.value, ($state.avgL)"
 	/* exponentially weighted average
 	    avg -= avg / N;
@@ -61,8 +57,8 @@ def illumHandler(evt) {
 	 when N=1 avg = the latest value. When N > 100 is approximately the number of previous values to average
 	 */	 
 	def float avL = state.avgL
-	avL -= avL / 200
-	avL += Float.parseFloat(evt.value) / 200
+	avL -= avL / NSample
+	avL += Float.parseFloat(evt.value) / NSample
 	state.avgL = avL
 	
 	if(vDevice.supportedCommands.find{it.toString() == "setIlluminance"}) { settings.vDevice.setIlluminance("${state.avgL.round(1)}"); sendEvent(name: "illuminance", value: state.avgL, displayed: true)  }
@@ -70,10 +66,11 @@ def illumHandler(evt) {
 }
 
 def tempHandler(evt) {
+	def NSample = numberOption as Integer
 	if (debugOutput) log.debug "tempHandler: $evt.device, $evt.value, ($state.avgT)"
 	def float avT = state.avgT
-	avT -= avT / 200
-	avT += Float.parseFloat(evt.value) / 200
+	avT -= avT / NSample
+	avT += Float.parseFloat(evt.value) / NSample
 	state.avgT = avT
 	
 	if(vDevice.supportedCommands.find{it.toString() == "setTemperature"}) { settings.vDevice.setTemperature("${state.avgT.round(1)}"); sendEvent(name: "temperature", value: state.avgT, displayed: true)  }
@@ -81,10 +78,11 @@ def tempHandler(evt) {
 }
 
 def humidHandler(evt) {
+	def NSample = numberOption as Integer
 	if (debugOutput) log.debug "humidHandler: $evt.device, $evt.value, ($state.avgH)"
 	def float avH = state.avgH
-	avH -= avH / 200
-	avH += Float.parseFloat(evt.value) / 200
+	avH -= avH / NSample
+	avH += Float.parseFloat(evt.value) / NSample
 	state.avgH = avH
 	
 	if(vDevice.supportedCommands.find{it.toString() == "setRelativeHumidity"}) { settings.vDevice.setRelativeHumidity("${state.avgH.round(1)}"); sendEvent(name: "humidity", value: state.avgH, displayed: true)  }
@@ -103,6 +101,7 @@ def updated() {
 	unsubscribe()
 	if (debugOutput) runIn(1800,logsOff)
 	if (debugOutput) log.debug "   Supported Commands of $vDevice:${vDevice.supportedCommands}"
+	schedule("0 0 8 ? * FRI *", updateCheck)
 	initialize()
 }
 
@@ -128,6 +127,8 @@ def mainPage()
 		section{paragraph "<div style='color:#1A77C9'>Calculate a Rolling Average of a set of Illuminance (Lux) sensors.</div>"    
 		input "vDevice", "capability.accelerationSensor", title: "Choose a Virtual Device to receive the Average.<i>(must support Illuminance)</i>"
 		input "illumSensors", "capability.illuminanceMeasurement", title: "Choose Illuminance Sensors include in an Average", multiple: true
+		input (name: "numberOption", type: "number", defaultValue: "10", range: "10..200", title: "Number of Samples to average.", description: "10 samples will be very responsive, while 200 samples is quite slow.", required: true)
+
 	}
       section (title: "<b>Name/Rename</b>") {
       	label title: "This child app's Name (optional)", required: false
@@ -139,7 +140,6 @@ def mainPage()
 
 
 def display() {
-    version()
     section{
 	   paragraph getFormat("line")
 	   paragraph "<div style='color:#1A77C9;text-align:center;font-weight:small;font-size:9px'>Developed by: C Steele<br/>Version Status: $state.status<br>Current Version: $state.version -  $state.Copyright</div>"
@@ -159,56 +159,65 @@ def getFormat(type, myText=""){
 }
 
 
-// Check Version   ***** with great thanks and acknowlegment to Cobra (CobraVmax) for his original code **************
-def version() {
-    updatecheck()
-    schedule("0 0 8 ? * FRI *", updatecheck)
+// Check Version   ***** with great thanks and acknowlegment to Cobra (CobraVmax) for his original code ****
+def updateCheck()
+{    
+	
+	def paramsUD = [uri: "https://hubitatcommunity.github.io/AverageThis/versions.json"]
+
+ 	asynchttpGet("updateCheckHandler", paramsUD) 
 }
 
+def updateCheckHandler(resp, data) {
 
-def updatecheck() {
-    setVersion()
-     def paramsUD = [uri: "https://hubitatcommunity.github.io/AverageThis/versions.json"]
-    try {
-     httpGet(paramsUD) { respUD ->
-         //  log.warn " Version Checking - Response Data: ${respUD.data}"   // Troubleshooting Debug Code 
-       	def copyrightRead = (respUD.data.copyright)
-       	state.Copyright = copyrightRead
-            def commentRead = (respUD.data.Comment)
-       	state.Comment = commentRead
-            def newVerRaw = (respUD.data.versions.Application.(state.InternalName))
-            state.newver = newVerRaw
-            def newVer = (respUD.data.versions.Application.(state.InternalName).replace(".", ""))
-       	def currentVer = state.version.replace(".", "")
-      	state.UpdateInfo = (respUD.data.versions.UpdateInfo.Application.(state.InternalName))
-            state.author = (respUD.data.author)
-           
-		if (newVer == "NLS"){
-                state.status = "<b>** This app is no longer supported by $state.author  **</b>"  
-                log.warn "** This app is no longer supported by $state.author **" 
-      	}           
-		else if (currentVer < newVer) {
-        	    state.status = "<b>New Version Available ($newVerRaw)</b>"
-        	    log.warn "** There is a newer version of this app available  (Version: $newVerRaw) **"
-        	    log.warn " Update: $state.UpdateInfo "
-                state.newBtn = state.status
-                state.updateMsg = "There is a new version of '$state.ExternalName' available (Version: $newVerRaw)"
-       	} 
-            else if (currentVer > newVer) {
-           	    state.status = "<b>You are using a Test version of this Driver (Version: $newVerRaw)</b>"
-            }
-		else { 
-      	    state.status = "Current"
-       	}
-     }
-    } 
-    catch (e) {
-        	log.error "Something went wrong: CHECK THE JSON FILE AND IT'S URI -  $e"
-    }
-    if (state.status != "Current") {
-		state.newBtn = state.status
-    }
-    else {
-        state.newBtn = "No Update Available"
-    }
+	state.InternalName = "AverageThisChild"
+
+	if (resp.getStatus() == 200 || resp.getStatus() == 207) {
+		respUD = parseJson(resp.data)
+		// log.warn " Version Checking - Response Data: $respUD"   // Troubleshooting Debug Code - Uncommenting this line should show the JSON response from your webserver 
+		state.Copyright = "${thisCopyright}"
+		def newVerRaw = (respUD.versions.Application.(state.InternalName))
+		def newVer = (respUD.versions.Application.(state.InternalName).replaceAll("[.vV]", ""))
+		def currentVer = version().replaceAll("[.vV]", "")   
+		state.UpdateInfo = (respUD.versions.UpdateInfo.Application.(state.InternalName))
+	
+		if(newVer == "NLS")
+		{
+		      state.Status = "<b>** This driver is no longer supported by $respUD.author  **</b>"       
+		      log.warn "** This driver is no longer supported by $respUD.author **"      
+		}           
+		else if(currentVer < newVer)
+		{
+		      state.Status = "<b>New Version Available (Version: $newVerRaw)</b>"
+		      log.warn "** There is a newer version of this Application available  (Version: $newVerRaw) **"
+		      log.warn "** $state.UpdateInfo **"
+		} 
+		else if(currentVer > newVer)
+		{
+		      state.Status = "<b>You are using a Test version of this Application (Version: $newVerRaw)</b>"
+		}
+		else
+		{ 
+		    state.Status = "Current"
+		    if (descTextEnable) log.info "You are using the current version of this driver"
+		}
+	
+	      if(state.Status == "Current")
+	      {
+	           state.UpdateInfo = "N/A"
+	           sendEvent(name: "CodeUpdate", value: state.UpdateInfo)
+	           sendEvent(name: "CodeStatus", value: state.Status)
+	      }
+	      else 
+	      {
+	           sendEvent(name: "CodeUpdate", value: state.UpdateInfo)
+	           sendEvent(name: "CodeStatus", value: state.Status)
+	      }
+      }
+      else
+      {
+           log.error "Something went wrong: CHECK THE JSON FILE AND IT'S URI"
+      }
 }
+
+def getThisCopyright(){"&copy; 2019 C Steele "}
